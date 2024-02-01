039a4d8daefab330b229a2809b0fbc3f4e3e79e6
Ndo18 <samuel.dmb@outlook.fr>
2024-02-02 00:51:54 +0100

    🐛 Fix "Nos réalisations" Date

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -29,17 +29,21 @@ const EventCard = ({
   );
 
 EventCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string,
   imageAlt: PropTypes.string,
-  date: PropTypes.instanceOf(Date).isRequired,
-  title: PropTypes.string.isRequired,
+  date: PropTypes.instanceOf(Date),
+  title: PropTypes.string,
   small: PropTypes.bool,
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
 };
 
 EventCard.defaultProps = {
+  imageSrc: "/images/stem-list-EVgsAbL51Rk-unsplash.png",
   imageAlt: "image",
   small: false,
+  title: "Conférence #productCON",
+  label: "soirée entreprise",
+  date: new(Date),
 }
 
 export default EventCard;
